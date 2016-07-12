813c16455138ed556184eb15be0a852013083b65
Don <aikdong@gmail.com>
2016-07-12 13:13:46 +0800

    dss

--- airplay.sh
+++ airplay.sh
@@ -13,45 +13,45 @@ fi
 
 case $1 in
     MORNING ) 
-    DAY=`date +%d`
-    PERIPH="--volume 36 /volume1/music/MORNING/$DAY.flac" 
+        DAY=`date +%d`
+        PERIPH="--volume 36 /volume1/music/MORNING/$DAY.flac" 
     ;;
     DOORBELL )
-    PERIPH="--volume 46 /volume1/music/Ringtone/dingding.m4a" 
+        PERIPH="--volume 46 /volume1/music/Ringtone/dingding.m4a" 
     ;;
     WEATHER )
-    PERIPH="--volume 46 /tmp/weather.mp3"
+        PERIPH="--volume 46 /tmp/weather.mp3"
     ;;
     PM25 )
-    PERIPH="--volume 46 /tmp/pm25.mp3"
+        PERIPH="--volume 46 /tmp/pm25.mp3"
     ;;
     CLOCK )
-    HOUR=`date +%H` 
-    PERIPH="--volume 25 /volume1/music/CLOCK/01.m4a"
+        HOUR=`date +%H` 
+        PERIPH="--volume 25 /volume1/music/CLOCK/01.m4a"
 esac
 
 # run the song
 /var/packages/AudioStation/target/sbin/synoairplayd --ip $AIRHOST --port $AIRPORT $PERIPH --foreground
 
 # case $1 in
-# MORNING ) 
-# sleep 10m 
-# ;;
-# DOORBELL )
-# sleep 15s
-# ;;
-# WEATHER )
-# sleep 15s
-# ;;
-# PM25 )
-# sleep 15s
-# ;;
-# CLOCK )
-# sleep 8s
+#     MORNING ) 
+#         sleep 10m 
+#     ;;
+#     DOORBELL )
+#         sleep 15s
+#     ;;
+#     WEATHER )
+#         sleep 15s
+#     ;;
+#     PM25 )
+#         sleep 15s
+#     ;;
+#     CLOCK )
+#         sleep 8s
 # esac
 
-# kill the new processes
-NEWPID=`cat $PIDPATH`
-if [ -s $NEWPID ]; then
-    kill $NEWPID
-fi
+# # kill the new processes
+# NEWPID=`cat $PIDPATH`
+# if [ -s $NEWPID ]; then
+#     kill $NEWPID
+# fi
