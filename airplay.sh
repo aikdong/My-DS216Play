748becc1b8fbac997a5145a954d9b73c47c73627
Don <aikdong@gmail.com>
2016-07-12 13:08:15 +0800

    modify airplay

--- airplay.sh
+++ airplay.sh
@@ -5,53 +5,53 @@ AIRPORT="5000"
 
 PIDPATH="/run/synoairplayd.$AIRHOST.$AIRPORT.pid" 
 
-# kill the old processes
-if [ -s $PIDPATH ]; then
+# kill the new processes
 NEWPID=`cat $PIDPATH`
-kill $NEWPID
+if [ -s $NEWPID ]; then
+    kill $NEWPID
 fi
 
 case $1 in
-MORNING ) 
-DAY=`date +%d`
-PERIPH="--ip $AIRHOST --port $AIRPORT --volume 36 /volume1/music/MORNING/$DAY.flac" 
-;;
-DOORBELL )
-PERIPH="--ip $AIRHOST --port $AIRPORT --volume 46 /volume1/music/Ringtone/dingding.m4a" 
-;;
-WEATHER )
-PERIPH="--ip $AIRHOST --port $AIRPORT --volume 46 /tmp/weather.mp3"
-;;
-PM25 )
-PERIPH="--ip $AIRHOST --port $AIRPORT --volume 46 /tmp/pm25.mp3"
-;;
-CLOCK )
-HOUR=`date +%H` 
-PERIPH="--ip $AIRHOST --port $AIRPORT --volume 25 /volume1/music/CLOCK/01.m4a"
+    MORNING ) 
+    DAY=`date +%d`
+    PERIPH="--volume 36 /volume1/music/MORNING/$DAY.flac" 
+    ;;
+    DOORBELL )
+    PERIPH="--volume 46 /volume1/music/Ringtone/dingding.m4a" 
+    ;;
+    WEATHER )
+    PERIPH="--volume 46 /tmp/weather.mp3"
+    ;;
+    PM25 )
+    PERIPH="--volume 46 /tmp/pm25.mp3"
+    ;;
+    CLOCK )
+    HOUR=`date +%H` 
+    PERIPH="--volume 25 /volume1/music/CLOCK/01.m4a"
 esac
 
 # run the song
-/var/packages/AudioStation/target/sbin/synoairplayd $PERIPH 
+/var/packages/AudioStation/target/sbin/synoairplayd --ip $AIRHOST --port $AIRPORT $PERIPH --foreground
 
-case $1 in
-MORNING ) 
-sleep 10m 
-;;
-DOORBELL )
-sleep 15s
-;;
-WEATHER )
-sleep 15s
-;;
-PM25 )
-sleep 15s
-;;
-CLOCK )
-sleep 8s
-esac
+# case $1 in
+# MORNING ) 
+# sleep 10m 
+# ;;
+# DOORBELL )
+# sleep 15s
+# ;;
+# WEATHER )
+# sleep 15s
+# ;;
+# PM25 )
+# sleep 15s
+# ;;
+# CLOCK )
+# sleep 8s
+# esac
 
 # kill the new processes
-if [ -s $PIDPATH ]; then
-NEWPID=`cat $PIDPATH` 
-kill $NEWPID
+NEWPID=`cat $PIDPATH`
+if [ -s $NEWPID ]; then
+    kill $NEWPID
 fi
